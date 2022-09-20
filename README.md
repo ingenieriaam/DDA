b2713bf7276257a8b0b3331a9dabd28903581fad
aortiz <ingenieria_am@outlook.com>
2022-09-19 23:59:19 -0300

    Updating project information

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # DDA
 Advanced Digital Design course material
 
+## Tools
+
+See the instructions for the makefile in the __verilog_test/sim_support__ folder for the use of these tools once installed.
+To generate folder structures, see the __make_folder__ file.
+___
+
+### <img src="https://terostechnology.github.io/terosHDLdoc/_static/logo.png" alt="drawing" width="30"/> Teros HDL: IDE
+
+> Open source IDE for HDL devlopers with functionalities commonly used by software developers.
+
+>https://terostechnology.github.io/terosHDLdoc/
+
+### <img src="https://wikiimg.tojsiabtv.com/wikipedia/en/thumb/0/00/XilinxVivado_Logo.jpg/440px-XilinxVivado_Logo.jpg" alt="drawing" width="70"/> : Synthesis and implementation 
+> It is reserved for synthesis and implementation since the workflow and time invested are cumbersome for its use.
+
+### <img src="https://upload.wikimedia.org/wikipedia/en/c/cb/Icarus_Verilog_logo2.png" alt="drawing" width="50"/> : Verilog synthesis & simulation
+> RTL level test compatible with Teros HDL
+
+> http://iverilog.icarus.com/
+
+### <img src="https://www.cocotb.org/assets/img/cocotb-logo.svg" alt="drawing" width="50"/> : Verilog synthesis & simulation using python
+> It allows to generate simulations with python in a very high level
+
+> https://docs.cocotb.org/en/stable/
+
+___
 ## FPGA board
 [Artix-35T](https://www.xilinx.com/products/boards-and-kits/arty.html)
 
--- verilog_tests/compuertas/compuertasLogicas_tb.v
+++ verilog_tests/compuertas/compuertasLogicas_tb.v
@@ -9,6 +9,8 @@ module compuertasLogicas_tb;
   reg   tb_act = 0;
   reg [2:0] tb_sel;
   wire sal;
+  // signals
+  reg [2:0] tb_ent;
 
   compuertasLogicas UUT 
   (
@@ -20,7 +22,10 @@ module compuertasLogicas_tb;
     .sal  ( sal )
   );
 
+  
   initial begin
+    $dumpfile("/home/aortiz/DDA/verilog_tests/compuertas/compuertasLogicas.vcd"); 
+    $dumpvars(0, compuertasLogicas_tb);
     tb_sel = 3'b000;
     tb_act = 1'b0;
     #10;
@@ -29,57 +34,34 @@ module compuertasLogicas_tb;
     tb_act = 1'b1;
     #10;
     tb_sel = 3'b001;
+    tb_ent = 3'b000;
     
     repeat(6) begin
-        #10;
-        $monitor("Ent1: %b | Ent2: %b | Ent3: %b | Salida: %b", tb_ent1,tb_ent2,tb_ent3,sal);
-        
-        //--------
-        tb_ent1=0;
-        tb_ent2=0;
-        tb_ent3=0;
-        #10;
-        //--------
-        tb_ent1=0;
-        tb_ent2=0;
-        tb_ent3=1;
-        #10;
-        //--------
-        tb_ent1=0;
-        tb_ent2=1;
-        tb_ent3=0;
-        #10;
-        //--------
-        tb_ent1=0;
-        tb_ent2=1;
-        tb_ent3=1;
-        #10;
-        //--------
-        tb_ent1=1;
-        tb_ent2=0;
-        tb_ent3=0;
-        #10;
-        //--------
-        tb_ent1=1;
-        tb_ent2=0;
-        tb_ent3=1;
-        #10;
-        //--------
-        tb_ent1=1;
-        tb_ent2=1;
-        tb_ent3=0;
-        #10;
-        //--------
-        tb_ent1=1;
-        tb_ent2=1;
-        tb_ent3=1;
-        #10;
-        //--------
-        tb_sel=tb_sel+1;
-        #10;
-    end
-    $finish;
+          #10;        
+          repeat(8)begin
+            {tb_ent1,tb_ent2,tb_ent3}=tb_ent;
+            $monitor("Ent1: %b | Ent2: %b | Ent3: %b | Salida: %b", tb_ent1,tb_ent2,tb_ent3,sal);
+            #20;
+            tb_ent=tb_ent+1;
+          end
+          tb_sel=tb_sel+1;
+          #10;
+      end
+      $finish;
+  end
 
+  always @(*) begin
+    case(tb_sel)
+      3'b001: begin
+                $display("SEL = AND");
+              end
+      3'b010: $display("SEL = OR");
+      3'b011: $display("SEL = XOR");
+      3'b100: $display("SEL = NAND");
+      3'b101: $display("SEL = NOR");
+      3'b110: $display("SEL = XNOR");
+      default: $display("SEL = OFF");
+    endcase
   end
 
 
