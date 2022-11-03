f633d4fe860ee7dd45a7b38c947734747beea415
aortiz <ingenieria_am@outlook.com>
2022-11-02 22:26:03 -0300

    ej4 unic test update

--- codigos/GP02/ej4/sim/fir_direct_tb.py
+++ codigos/GP02/ej4/sim/fir_direct_tb.py
@@ -27,22 +27,31 @@ async def run_test(dut):
     ref=float(-1*rang)
     refq=ref/rang
     x=np.array([refq for i in range(8)])
+
+    # I take the internal values of the circuit
     h=np.array([float(dut.i_h[i].value)/ rang for i in range(dut.NTAPS.value)])
     a=np.array([1.,0.,0.,0.])
 
+    # reference filtering
     y2=sp.signal.lfilter(h,a,x)
     dut._log.info("differential eq output:")
     dut._log.info(y2)
     o_y=[]
+    
+    # test
     for i in range(len(x)):
         dut.i_x.value =int(ref)
         await Timer(PERIOD, units='ns')
         # check for another std values like X,U,Z.
         if str(dut.o_y.value).isdigit():
-            dut._log.info("Y @cycle %d = %f" %(i, (dut.o_y.value.signed_integer/rang) ))
-            o_y.append((dut.o_y.value.signed_integer/rang))
+            dut_res = (dut.o_y.value.signed_integer/rang)
+            dut._log.debug("Y @cycle %d = %f" %(i, dut_res ))
+            o_y.append(dut_res)
+            assert dut_res==y2[i] ,"FAIL: o_mul must be {ref} and was {res}".format(
+                            res=dut_res,ref=y2[i])
         else:
-            dut._log.info("Y @cycle %d = %s" %(i, dut.o_y.value ))
+            dut._log.debug("Y @cycle %d = %s" %(i, dut.o_y.value ))
+            assert False, "Result was not a number"
 
     dut._log.info("DUT output:")
     dut._log.info(o_y)
